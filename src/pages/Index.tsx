88c36e2b0a640ef3f0884ae8cac93b4020ec48ac
Your Name <your@email.com>
2026-04-28 09:51:20 +0530

    git commit -m "add hero component "

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,11 @@
+import { Hero } from "@/components/main/Hero";
 import {Navbar}  from "@/components/main/Navbar";
 const Index = () => {
   return (
     <main className="min-h-screen bg-background text-foreground overflow-x-hidden">
       <h1 className="sr-only">Chain Pilot — Autonomous chain Portfolio AI</h1>
       <Navbar />
+      <Hero/>
       
     </main>
   );
